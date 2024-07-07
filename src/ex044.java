299fb4736f592802567085639c061a4003586cc2
ruannino <ruanfariasdc@hotmail.com>
2024-07-06 23:14:57 -0300

    Update 06.07.2024

--- src/ex044.java
+++ src/ex044.java
@@ -7,6 +7,7 @@ como qual dos quartos ele escolheu (de 0 a 9). Suponha que seja escolhido um
 quarto vago. Ao final, seu programa deve imprimir um relatório de todas ocupaçoes
 do pensionato, por ordem de quarto, conforme exemplo.
 Ex Level Medium*/
+import entities.Rent;
 import java.util.Scanner;
 import java.util.Locale;
 public class ex044 {
@@ -14,8 +15,29 @@ public class ex044 {
         Locale.setDefault(Locale.US);
         Scanner teclado = new Scanner(System.in);
 
+        System.out.print("Hom many rooms will be rented? ");
+        int n = teclado.nextInt();
+        teclado.nextLine();
 
+        Rent[] quartos = new Rent[10];
 
+        for (int q = 0; q < n; q++) {
+            System.out.printf("Rent #%d: \n", q+1);
+            System.out.print("Name: ");
+            String name = teclado.nextLine();
+            System.out.print("Email: ");
+            String email = teclado.next();
+            System.out.print("Room: ");
+            int roomNumber = teclado.nextInt();
+            teclado.nextLine();
+            quartos[roomNumber] = new Rent(name, email);
+        }
+        System.out.println("Busy rooms: ");
+        for (int q = 0; q < quartos.length; q++) {
+            if (quartos[q] != null) {
+                System.out.printf("%d: %s\n", q, quartos[q]);
+            }
+        }
         teclado.close();
     }
 }
